a90662ea09344389ce3507e812dc8c67bb5da1d4
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-28 00:36:07 +0500

    netlify serverless react completed

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
     <h2>To deploy on netlify serverless,first simply deploy react app on netlify</h2>
     <h2>Then define the functions directory in netlify.toml file</h2>
     <h2>create the netlify function </h2>
+    <h2>create the netlify function </h2>
     <h2>netlify.toml file does not mattter. just tell your location on github </h2>
     <h1>{state.message}</h1>
   
