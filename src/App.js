29341faf7255b486d3abd2782fd6c01316062375
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-27 23:56:24 +0500

    netlify react app serverless

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
     <h2>To deploy on netlify serverless,first simply deploy react app on netlify</h2>
     <h2>Then define the functions directory in netlify.toml file</h2>
-    <h2>create the netlify function lj;lj</h2>
+    <h2>create the netlify function lj;adfadf</h2>
     {/* <h1>{state.message}</h1> */}
   
     </div>
