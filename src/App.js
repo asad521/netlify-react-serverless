9a5c0ea69ae10090dece6b07e66fd969b5d555a8
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-27 23:46:13 +0500

    serverless function on netlify react comp

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import React,{useEffect,useState} from 'react'
 
 function App() {
   const [state,setState] =useState(0)
-  useEffect(async() => {
-    try {
-      const res = await fetch(`.netlify/functions/func2sls?name=from serverless function`)
-      const data =await  res.json()
-      console.log(data)
-      console.log('in use effect')
-      setState(data)
-    } catch (error) {
+  // useEffect(async() => {
+  //   try {
+  //     const res = await fetch(`.netlify/functions/func2sls?name=from serverless function`)
+  //     const data =await  res.json()
+  //     console.log(data)
+  //     console.log('in use effect')
+  //     setState(data)
+  //   } catch (error) {
       
-    }
-  },[])
+  //   }
+  // },[])
   return (
     <div className="App">
     <h2>To deploy on netlify serverless,first simply deploy react app on netlify</h2>
