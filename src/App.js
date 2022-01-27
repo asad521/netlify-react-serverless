82b5f136750676f7275115d880d9a303e3080c8b
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-28 00:33:13 +0500

    netlify serverless react completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 
 
 function App() {
-  // const [state,setState] =useState(0)
-    // useEffect(async() => {
-    //   try {
-    //     const res = await fetch(`.netlify/functions/func2sls?name=from serverless function`)
-    //     const data =await  res.json()
-    //     console.log(data)
-    //     console.log('in use effect')
-    //     setState(data)
-    //   } catch (error) {
+  const [state,setState] =useState(0)
+    useEffect(async() => {
+      try {
+        const res = await fetch(`.netlify/functions/slsfunc?name=from serverless function trigered in use effect`)
+        const data =await  res.json()
+        console.log(data)
+        console.log('in use effect')
+        setState(data)
+      } catch (error) {
         
-    //   }
-    // },[])
+      }
+    },[])
   return (
     <div className="App">
     <h2>To deploy on netlify serverless,first simply deploy react app on netlify</h2>
     <h2>Then define the functions directory in netlify.toml file</h2>
-    <h2>create the netlify function lj;adfadf</h2>
-    {/* <h1>{state.message}</h1> */}
+    <h2>create the netlify function </h2>
+    <h2>netlify.toml file does not mattter. just tell your location on github </h2>
+    <h1>{state.message}</h1>
   
     </div>
   )
