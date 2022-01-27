aac829e422268d65e670b19c1fc2b7632bd3f956
asad521 <63185364+asad521@users.noreply.github.com>
2022-01-27 18:18:38 +0500

    netlify build reacct web with serverless

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h2>To deploy on netlify with serverless,do first simply deploy react app on netlify</h2>
+    <h2>To deploy on netlify with serverless,dooo first simply deploy react app on netlify</h2>
     </div>
   );
 }
